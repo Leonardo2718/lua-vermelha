730f01611b1bf288693344014c93115c2f4974f5
Leonardo Banderali <leob@linux.vnet.ibm.com>
2016-11-17 11:05:33 -0500

    Update ltypedictionary to use new struct API
    Update ltypedictionary to use the new JitBuilder struct definition API.
    Since all structs defined are overlays of VM structs, it makes sense to
    use the safer API. For now though, ignore cases involving unions inside
    structs and the like as the behaviour of these cases still needs to be
    tested and verified to do the correct thing.

--- vermelha/ltypedictionary.cpp
+++ vermelha/ltypedictionary.cpp
@@ -1,6 +1,6 @@
 #include "ltypedictionary.hpp"
 #include "luavm.hpp"
-#include <stdio.h>
+
 Lua::TypeDictionary::TypeDictionary() : TR::TypeDictionary() {
    // common lua types
    luaTypes.lu_byte         = Int8;
@@ -25,52 +25,52 @@ Lua::TypeDictionary::TypeDictionary() : TR::TypeDictionary() {
    // lobject.h types ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
    // struct TValue
-   luaTypes.TValue = DefineStruct("TValue");
-   DefineField("TValue", "value_", Int64); // this should actually be a union
-   DefineField("TValue", "tt_", Int32);
-   DefineField("TValue", "__padding0__", Int32);
-   CloseStruct("TValue");
+   luaTypes.TValue = DEFINE_STRUCT(TValue);
+   DEFINE_FIELD(TValue, value_, Int64); // this should actually be a union
+   DEFINE_FIELD(TValue, tt_, Int32);
+   //DefineField("TValue", "__padding0__", Int32);
+   CLOSE_STRUCT(TValue);
 
    luaTypes.StkId = PointerTo("TValue"); // stack index
 
    // struct LClosure
-   luaTypes.LClosure = DefineStruct("LClosure");
-   DefineField("LClosure", "next", pGCObject_t, offsetof(LClosure, next));                // ClosureHeader  CommonHeader
-   DefineField("LClosure", "tt", luaTypes.lu_byte, offsetof(LClosure, tt));               //      |              |
-   DefineField("LClosure", "marked", luaTypes.lu_byte, offsetof(LClosure, marked));       //      |              |
-   DefineField("LClosure", "nupvalues", luaTypes.lu_byte, offsetof(LClosure, nupvalues)); //      |
-   DefineField("LClosure", "gclist", pGCObject_t, offsetof(LClosure, gclist));            //      |
-   DefineField("LClosure", "p", pProto_t, offsetof(LClosure, p));
-   DefineField("LClosure", "upvals", Address, offsetof(LClosure, upvals));
-   CloseStruct("LClosure");
+   luaTypes.LClosure = DEFINE_STRUCT(LClosure);
+   DEFINE_FIELD(LClosure, next, pGCObject_t);           // ClosureHeader  CommonHeader
+   DEFINE_FIELD(LClosure, tt, luaTypes.lu_byte);        //      |              |
+   DEFINE_FIELD(LClosure, marked, luaTypes.lu_byte);    //      |              |
+   DEFINE_FIELD(LClosure, nupvalues, luaTypes.lu_byte); //      |
+   DEFINE_FIELD(LClosure, gclist, pGCObject_t);         //      |
+   DEFINE_FIELD(LClosure, p, pProto_t);
+   DEFINE_FIELD(LClosure, upvals, Address);
+   CLOSE_STRUCT(LClosure);
 
    // lfunc.h types ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-   luaTypes.UpVal = DefineStruct("UpVal");
-   DefineField("UpVal", "v", PointerTo(luaTypes.TValue), offsetof(UpVal, v));  // points to stack or to its own value
-   DefineField("UpVal", "refcount", Int64, offsetof(UpVal, refcount));         // reference counter
+   luaTypes.UpVal = DEFINE_STRUCT(UpVal);
+   DEFINE_FIELD(UpVal, v, PointerTo(luaTypes.TValue));  // points to stack or to its own value
+   DEFINE_FIELD(UpVal, refcount, Int64);                // reference counter
 /*
    union {
-      struct {                                                                 // (when open)
-         UpVal* next;                                                          // linked list
-         int touched;                                                          // mark to avoid cycles with dead threads
+      struct {                                          // (when open)
+         UpVal* next;                                   // linked list
+         int touched;                                   // mark to avoid cycles with dead threads
       } open;
 */
    DefineField("UpVal", "u_value", luaTypes.TValue, offsetof(UpVal, u.value)); /* TValue value; */ // the value (when closed)
 /*
    } u;
 */
-   CloseStruct("UpVal", sizeof(UpVal));
+   CLOSE_STRUCT(UpVal);
 
    // lstate.h types ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
    // struct CallInfo (Information about a call)
-   luaTypes.CallInfo = DefineStruct("CallInfo");
+   luaTypes.CallInfo = DEFINE_STRUCT(CallInfo);
    TR::IlType* pCallInfo_t = PointerTo("CallInfo");
-   DefineField("CallInfo", "func", luaTypes.StkId);  // function index in the stack
-   DefineField("CallInfo", "top", luaTypes.StkId);   // top for this function
-   DefineField("CallInfo", "previous", pCallInfo_t); // dynamic call link
-   DefineField("CallInfo", "next", pCallInfo_t);     //       |
+   DEFINE_FIELD(CallInfo, func, luaTypes.StkId);  // function index in the stack
+   DEFINE_FIELD(CallInfo, top, luaTypes.StkId);   // top for this function
+   DEFINE_FIELD(CallInfo, previous, pCallInfo_t); // dynamic call link
+   DEFINE_FIELD(CallInfo, next, pCallInfo_t);     //       |
 /*
    union { // Node: JitBuilder does not currently support defining unions
        struct {   // only for Lua functions
@@ -88,38 +88,38 @@ Lua::TypeDictionary::TypeDictionary() : TR::TypeDictionary() {
        } c;
    } u;
 */
-   DefineField("CallInfo", "extra", PtrDiff_t);
-   DefineField("CallInfo", "nresults", Word);
-   DefineField("CallInfo", "callstatus", luaTypes.lu_byte);
-   CloseStruct("CallInfo");
+   DEFINE_FIELD(CallInfo, extra, PtrDiff_t);
+   DEFINE_FIELD(CallInfo, nresults, Int16);
+   DEFINE_FIELD(CallInfo, callstatus, luaTypes.lu_byte);
+   CLOSE_STRUCT(CallInfo);
 
    // struct lua_State (per thread state)
-   luaTypes.lua_State = DefineStruct("lua_State");
+   luaTypes.lua_State = DEFINE_STRUCT(lua_State);
    TR::IlType* pLuaState_t = PointerTo("lua_State");
-   DefineField("lua_State", "next", pGCObject_t);             // CommonHeader
-   DefineField("lua_State", "tt", luaTypes.lu_byte);          //      |
-   DefineField("lua_State", "marked", luaTypes.lu_byte);      //      |
-   DefineField("lua_State", "nci", Int16);                    // number of items in `ci` list (should be uint16_t ?)
-   DefineField("lua_State", "status", luaTypes.lu_byte);
-   DefineField("lua_State", "top", luaTypes.StkId);           // first free slot in the stack
-   DefineField("lua_State", "l_G", pGlobalState_t);
-   DefineField("lua_State", "ci", pCallInfo_t);               // call info for current function
-   DefineField("lua_State", "oldpc", pInstruction);           // last pc traced
-   DefineField("lua_State", "stack_last", luaTypes.StkId);    // last free slot in the stack
-   DefineField("lua_State", "stack", luaTypes.StkId);         // stack base
-   DefineField("lua_State", "openupval", pUpVal_t);           // list of open upvalues in this stack
-   DefineField("lua_State", "gclist", pGCObject_t);
-   DefineField("lua_State", "twups", pLuaState_t);            // list of threads with open upvalues
-   DefineField("lua_State", "errorJmp", pLuaLongjmp);         // current error recover point
-   DefineField("lua_State", "base_ci", luaTypes.CallInfo);    // CallInfo for first level (C calling Lua)
-   DefineField("lua_State", "lua_Hook", lua_Hook_t);          // (should be `volatile` ?)
-   DefineField("lua_State", "errfunc", PtrDiff_t);
-   DefineField("lua_State", "stacksize", Int32);
-   DefineField("lua_State", "basehookcount", Int32);
-   DefineField("lua_State", "hookcount", Int32);
-   DefineField("lua_State", "nny", Int16);                    // number of non-yieldable calls in stack
-   DefineField("lua_State", "nCcalls", Int16);                // number of nested C calls
-   DefineField("lua_State", "hookmask", l_signalT_t);
-   DefineField("lua_State", "allowhook", luaTypes.lu_byte);
-   CloseStruct("lua_State");
+   DEFINE_FIELD(lua_State, next, pGCObject_t);             // CommonHeader
+   DEFINE_FIELD(lua_State, tt, luaTypes.lu_byte);          //      |
+   DEFINE_FIELD(lua_State, marked, luaTypes.lu_byte);      //      |
+   DEFINE_FIELD(lua_State, nci, Int16);                    // number of items in `ci` list (should be uint16_t ?)
+   DEFINE_FIELD(lua_State, status, luaTypes.lu_byte);
+   DEFINE_FIELD(lua_State, top, luaTypes.StkId);           // first free slot in the stack
+   DEFINE_FIELD(lua_State, l_G, pGlobalState_t);
+   DEFINE_FIELD(lua_State, ci, pCallInfo_t);               // call info for current function
+   DEFINE_FIELD(lua_State, oldpc, pInstruction);           // last pc traced
+   DEFINE_FIELD(lua_State, stack_last, luaTypes.StkId);    // last free slot in the stack
+   DEFINE_FIELD(lua_State, stack, luaTypes.StkId);         // stack base
+   DEFINE_FIELD(lua_State, openupval, pUpVal_t);           // list of open upvalues in this stack
+   DEFINE_FIELD(lua_State, gclist, pGCObject_t);
+   DEFINE_FIELD(lua_State, twups, pLuaState_t);            // list of threads with open upvalues
+   DEFINE_FIELD(lua_State, errorJmp, pLuaLongjmp);         // current error recover point
+   DEFINE_FIELD(lua_State, base_ci, luaTypes.CallInfo);    // CallInfo for first level (C calling Lua)
+   DEFINE_FIELD(lua_State, hook, lua_Hook_t);              // (should be `volatile` ?)
+   DEFINE_FIELD(lua_State, errfunc, PtrDiff_t);
+   DEFINE_FIELD(lua_State, stacksize, Int32);
+   DEFINE_FIELD(lua_State, basehookcount, Int32);
+   DEFINE_FIELD(lua_State, hookcount, Int32);
+   DEFINE_FIELD(lua_State, nny, Int16);                    // number of non-yieldable calls in stack
+   DEFINE_FIELD(lua_State, nCcalls, Int16);                // number of nested C calls
+   DEFINE_FIELD(lua_State, hookmask, l_signalT_t);
+   DEFINE_FIELD(lua_State, allowhook, luaTypes.lu_byte);
+   CLOSE_STRUCT(lua_State);
 }
